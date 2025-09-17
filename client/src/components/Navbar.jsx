db94bd92d09ed2596614780cccbc1f8a090ed88e
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2025-09-17 15:07:28 +0530

    Authentication added using clerk

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   // UseNavigate hoook is use for navigations; When the logo img clicked, will be re-directed to home page
   const navigate = useNavigate();
+  const { user } = useUser(); //A hook that gives the currently signed-in user.
+  const { openSignIn } = useClerk(); //A hook that gives access to Clerk’s API, like opening the sign-in modal.
   return (
     <div className="fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
       <img
@@ -14,9 +16,19 @@ const Navbar = () => {
         className="w-32 sm:w-44 cursor-pointer"
         onClick={() => navigate("/")}
       />
-      <button className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">
-        Get started <ArrowRight className="w-4 h-4" />
-      </button>
+      {/* Must wrap any JavaScript logic (like ternaries, function calls, variables) inside {} in JSX. 
+          Ifweu write without curly braces:React will throw an error, because JSX doesn’t know how to handle raw JS without {}. */}
+      {user ? (
+        // UserButton is a ready-made component that shows the user’s profile picture and a dropdown menu (sign out, manage account)
+        <UserButton />
+      ) : (
+        <button
+          onClick={openSignIn}
+          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+        >
+          Get started <ArrowRight className="w-4 h-4" />
+        </button>
+      )}
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,21 @@ import { createRoot } from "react-dom/client"; // new way to initialize a React 
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+//Clerk is an authentication and user management library.
+// It provides: Sign up / Sign in flows, Social login (Google), User profile management, Session handling
+
+// Import publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
 );
