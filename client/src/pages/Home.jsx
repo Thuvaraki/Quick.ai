5bf26033da77c4e00d765d3432d3341882fa2cf2
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2025-09-17 19:37:51 +0530

    Plan component added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,15 +2,17 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AiTools from "../components/AiTools";
 import Testimonial from "../components/Testimonial";
+import Plan from "../components/Plan";
 
 const Home = () => {
   return (
-    // fragment doesn’t add an extra <div> wrapper in the rendered HTML.
+    // <> </> fragment doesn’t add an extra <div> wrapper in the rendered HTML.
     <>
       <Navbar />
       <Hero />
       <AiTools />
       <Testimonial />
+      <Plan />
     </>
   );
 };
