bf179ea18b19457fe171675a6cbbbaa947f2dea2
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2025-09-18 13:10:39 +0530

    Sidebar added

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,13 +1,48 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import { SignIn, useUser } from "@clerk/clerk-react";
 
 const Layout = () => {
-  return (
-    <div>
-      <h1>Layout</h1>
-      {
-        <Outlet /> /* acts as a placeholder for child routes inside a parent route. */
-      }
+  const navigate = useNavigate();
+  const [sidebarProp, setSidebarProp] = useState(false);
+  const { user } = useUser();
+
+  // logged in user is only able to access the page
+  return user ? (
+    <div className="flex flex-col items-start justify-start h-screen">
+      <nav className="w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200">
+        <img
+          className="cursor-pointer w-32 sm:w-44"
+          src={assets.logo}
+          alt=""
+          onClick={() => navigate("/")}
+        />
+        {sidebarProp ? (
+          <X
+            onClick={() => setSidebarProp(false)}
+            className="w-6 h-6 text-gray-600 sm:hidden"
+          />
+        ) : (
+          <Menu
+            onClick={() => setSidebarProp(true)}
+            className="w-6 h-6 text-gray-600 sm:hidden"
+          />
+        )}
+      </nav>
+      <div className="flex-1 w-full flex h-[calc(100vh-64px)]">
+        <Sidebar sidebarProp={sidebarProp} setSidebarProp={setSidebarProp} />
+        <div className="flex-1 bg-[#F4F7FB]">
+          <p>Outlet works!</p>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <SignIn />
     </div>
   );
 };
